db2afb80c36f4ebdc853cbbeb202ec1711c84dcc
Mohammed Farooq <mohammedfarooq.devops@gmail.com>
2026-02-27 00:50:59 +0530

    Update EKS cluster version to 1.32

--- infra-tf/main.tf
+++ infra-tf/main.tf
@@ -37,7 +37,7 @@ module "eks" {
   version = "~> 20.31"
 
   cluster_name    = "end-to-end-devsecops"
-  cluster_version = "1.31"
+  cluster_version = "1.32"
 
   # Optional
   cluster_endpoint_public_access = true
